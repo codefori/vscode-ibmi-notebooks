c838df66cb2f31a03fd9a04c0a97a0b4f4afbd04
Liam Barry Allan <liam@worksofbarry.com>
2022-03-06 12:53:13 -0500

    Command to open Notebook

--- src/extension.ts
+++ src/extension.ts
@@ -9,14 +9,21 @@ import IBMiController from './Controller';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-ibmi-notebooks" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(`Congratulations, your extension "vscode-ibmi-notebooks" is now active!`);
 
-	context.subscriptions.push(
-		vscode.workspace.registerNotebookSerializer('ibmi-notebook', new IBMiSerializer()),
-		new IBMiController()
-	);
+  const openNotebook = vscode.commands.registerCommand(`vscode-ibmi-notebooks.open`, (node) => {
+    const uri = node ? node.resourceUri : vscode.Uri.parse(`untitled:` + `notebook.inb`);
+
+    vscode.commands.executeCommand(`vscode.openWith`, uri, `ibmi-notebook`);
+  });
+
+  context.subscriptions.push(
+    vscode.workspace.registerNotebookSerializer(`ibmi-notebook`, new IBMiSerializer()),
+    new IBMiController(),
+    openNotebook
+  );
 }
 
 // this method is called when your extension is deactivated
